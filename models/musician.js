ccf3caa0f534d9414d664fcdb269dc74b696f5e2
micha trieba <micha.trieba@mayflower.de>
2016-12-30 14:47:32 +0100

    A musician can have a band

--- models/musician.js
+++ models/musician.js
@@ -2,7 +2,8 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 var musicianSchema = Schema({
-		name: String
+		name: String,
+    band: { type: Schema.Types.ObjectId, ref: 'Band' }
 	});
 
 module.exports = mongoose.model('Musician', musicianSchema);
--- routes/musicians.js
+++ routes/musicians.js
@@ -3,9 +3,12 @@ var router = express.Router();
 var get_object_id = require('../helper');
 
 var Musician = require('../models/musician');
+var Band = require('../models/band');
 
 router.get('/', function(req, res, next) {
-  Musician.find({}, function (err, docs) {
+  Musician.find({})
+    .populate('band')
+    .exec(function (err, docs) {
     res.render('musicians', { title: 'Musicians', musicians: docs});
   });
 });
@@ -26,19 +29,31 @@ router.post('/new', function(req, res, next) {
 });
 
 router.get('/edit/:id', function(req, res, next) {
+  Band.find({}, function(error, bands) {
+    Musician.findById(get_object_id(req.params.id))
+                      .populate('band')
+                      .exec(function(err, docs) {
 
-  Musician.findById(get_object_id(req.params.id), function(err, docs) {
-    if(err) {
-      console.log("was not able to find a musician for id %s with: ", object_id, err)
-    }
-    res.render('musician_edit', { title: "Edit musician", musician: docs });
+      if (error) {
+        console.log("Could not fetch band list");
+      }
+      var bandsList = [];
+      bandsList = bands;
+
+      if(err) {
+        console.log("was not able to find a musician for id %s with: ", object_id, err)
+      }
+
+      res.render('musician_edit', { title: "Edit musician", musician: docs, bands: bandsList });
+    });
   });
 });
 
 router.post('/update/:id', function(req, res,next) {
     Musician.findById(get_object_id(req.params.id), function(err, musician) {
-    musician.name = req.body.name;
-    musician.save(function(err, docs) {
+      musician.name = req.body.name;
+      musician.band = req.body.band_id;
+      musician.save(function(err, docs) {
       if (err) {
         res.send('unable to save musician');
       }
