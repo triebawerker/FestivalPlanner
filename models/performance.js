afe57bc37c75e62d51add34c6d8bd8df80d04ace
micha trieba <micha.trieba@mayflower.de>
2017-01-16 09:01:37 +0100

    performance has a description

--- models/performance.js
+++ models/performance.js
@@ -7,10 +7,10 @@ var performanceSchema = Schema({
   from:        Date,
   to:          Date,
   featuring:   String,
+  description: String,
   festival:    { type: Schema.Types.ObjectId, ref: 'Festival' },
   location:    { type: Schema.Types.ObjectId, ref: 'Location' },
   band:        { type: Schema.Types.ObjectId, ref: 'Band' }
 });
 
 module.exports = mongoose.model('performance', performanceSchema);
-
