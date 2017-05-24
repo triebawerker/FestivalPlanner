e0f50f61afbb01022586b569b189b5662ab189be
triebawerker <micha@triebawerker.de>
2017-05-24 09:18:52 +0200

    use multiple origins

--- common.js
+++ common.js
@@ -2,9 +2,9 @@ exports.config = function() {
   var env=(process.env.NODE_ENV || "development")
   var host;
   if(env == "development") {
-    host='http://localhost:4200';
+    host=['http://localhost:4200'];
   } else if (env == "production") {
-    host= 'http://46.101.225.110'
+    host=['http://46.101.225.110', 'uckermark-jazzfest.de', 'www.uckermark-jazzfest.de']
   }
   return host;
 }
