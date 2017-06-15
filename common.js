67dabf01f5d0174e0bc7a753319c4ddbb36f3eff
triebawerker <micha@triebawerker.de>
2017-06-15 19:15:40 +0200

    only check for env on develompment

--- common.js
+++ common.js
@@ -1,7 +1,6 @@
 exports.config = function() {
-  var env=(process.env.NODE_ENV || "development")
   var host;
-  if(env == "development") {
+  if(process.env.NODE_ENV == "development") {
     host=['http://localhost:4200'];
   } else {
     host=['http://uckermark-jazzfest.de']
